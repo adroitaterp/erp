7ce3c073f7b1f29d26193718685fa1fa06dd503c
adroitaterp <115502255+adroitaterp@users.noreply.github.com>
2022-10-14 10:51:48 +0400

    sale custom

---
+++ sale_custom/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale Custom",
+
+    'summary': """
+        Sale Custom""",
+
+    'description': """
+        Sale Custom
+    """,
+
+    'author': "Musadiq Fiaz Chaudhary",
+    'website': "http://www.musadiqch.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'sale',
+    'version': '15.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale', 'hr'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'views/sale_views.xml',
+    ],
+
+}
