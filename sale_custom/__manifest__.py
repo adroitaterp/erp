fde294a7066bfab738216aeedbd7277bc215cb03
adroitaterp <115502255+adroitaterp@users.noreply.github.com>
2022-12-08 20:52:39 +0400

    sale_email

--- sale_custom/__manifest__.py
+++ sale_custom/__manifest__.py
@@ -25,7 +25,7 @@
     'data': [
         'security/ir.model.access.csv',
         'security/security.xml',
-        'data/mail_template.xml',
+        # 'data/mail_template.xml',
         'views/lead_views.xml',
         'views/sale_views.xml',
         'reports/sale_proposal_report.xml',
