fefa03ed6b62a3678e80f585394691a5c58329cb
adroitaterp <115502255+adroitaterp@users.noreply.github.com>
2023-04-27 12:27:30 -0700

    prod

---
+++ inherit_product/inherit_product/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+from . import models
+from . import email_cc
+from . import project_type
---
+++ inherit_product/inherit_product/models/models.py
@@ -0,0 +1,46 @@
+from odoo import models, fields, api
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    is_percentage_price = fields.Boolean(string='Is Percentage Price')
+    percentage_price = fields.Float(string='Percentage Price', default="1")
+
+
+class PricelistItem(models.Model):
+    _inherit = 'product.pricelist.item'
+
+    @api.depends('applied_on', 'compute_price')
+    def _compute_price(self):
+        for item in self:
+            if item.product_tmpl_id.is_percentage_price:
+                item.price = item.product_tmpl_id.percentage_price
+                item.product_id and item.product_id.product_tmpl_id.sudo().write({'list_price': item.price})
+            else:
+                super(PricelistItem, item)._compute_price()
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    percentage_price = fields.Float(string='Percentage Price', related='product_tmpl_id.percentage_price')
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    percentage_price = fields.Float(string='Percentage Price' , related='product_id.percentage_price')
+    
+    # @api.depends('percentage_price')
+    # def _compute_unit_price(self):
+    #     for line in self:
+    #         if line.percentage_price:
+    #             line.price_unit = 0
+    #         else:
+    #             line.price_unit = line.price_unit
+
+
+
+
+
