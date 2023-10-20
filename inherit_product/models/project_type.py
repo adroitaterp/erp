2899356636c0da3b9fb55027a9757c2ef580314c
hamayunfazal <hamayunfazal1@gmail.com>
2023-10-20 12:51:46 +0000

    changes in project inherit module

--- inherit_product/models/project_type.py
+++ inherit_product/models/project_type.py
@@ -1,18 +1,39 @@
 from odoo import api, fields, models, _
 from odoo.exceptions import ValidationError
 from datetime import datetime
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ProjectType(models.Model):
     _name = 'project.type'
 
     name = fields.Char(string="Type Name")
-
-
 class ProjectProject(models.Model):
     _inherit = 'project.project'
-
     project_type_id = fields.Many2one('project.type', string="Project Type")
+    
+    @api.onchange('department_project_id')
+    def branch_filter(self):
+        aa=self.department_project_id.project_by_id
+        all=[]
+        for j in aa:
+            all.append(j.id)
+        if all:
+           
+            return {'domain': {'project_type_id': [('id', 'in', all)]}}
+        else:
+         
+            return {'domain': {'project_type_id': [('id', 'in', [])]}}
+
+       
+
+        # if all:
+        #     return {'domain': {'project_type_id':[('id','in',[all])]}}
+        # else:
+        #     return {'domain': {'project_type_id':[('id','in',[])]}}
+            
+
 
 
 class ProjectTask(models.Model):
