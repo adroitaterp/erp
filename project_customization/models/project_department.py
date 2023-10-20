96a3a01dd52fe60d48a8744db1e7c6a99bd8cdfe
hamayunfazal <hamayunfazal1@gmail.com>
2023-10-20 12:46:53 +0000

    changes in project customization

--- project_customization/models/project_department.py
+++ project_customization/models/project_department.py
@@ -6,7 +6,7 @@ from odoo import models, fields, api
 class ProjectProjectInherit(models.Model):
     _name = 'project.department'
     name = fields.Char(string="Department")
-    request_by_id = fields.Many2many('project.project', string='Project Type', store=True)
+    project_by_id = fields.Many2many('project.type', string='Project Type', store=True)
     
 
 
