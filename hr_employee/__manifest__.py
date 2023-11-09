bfcab01e422dab738523969dce8630d20bd1f348
hamayun fazal <hamayunfazal1@gmail.com>
2023-11-09 19:14:40 +0000

    new hr employee module

---
+++ hr_employee/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "HR EMPLOYEE",
+
+    'summary': """
+    new addation inhr employee module
+        """,
+
+    'description': """
+        new addation inhr employee module
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/hr_employee.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+    'license': 'AGPL-3',
+}
---
+++ hr_employee/models/hr_employee.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api , Command
+
+
+class HrEmployeeInherit(models.Model):
+     _inherit = "hr.employee"
+     date_of_joining = fields.Date('Date of Joining')
+     passport = fields.Selection([
+        ('yes', 'Yes'),('no', 'No'),],
+        default='', string='Passport')
+     passport_number = fields.Char(string="Passport Number")
+     passport_expiration_date = fields.Date('Passport Expiration')
+     Online_Offer_letter = fields.Char(string="Online Offer Letter No")
+     visa_change_status_no = fields.Char(string="Visa Change Status No")
+     visa_title = fields.Char(string="Visa Title")
+     labour_card_no = fields.Char(string="Labour Card No")
+     labour_card_expiry = fields.Date('Labour Card Expiry')
+     labour_contract_no = fields.Char(string="Labour Contract No")
+     work_permit_no = fields.Char(string="Work Permit No")
+     employment_visa_no = fields.Char(string="Employment Visa No")
+     employment_visa_start_date = fields.Date('Employment Visa Start Date')
+     employment_visa_end_date = fields.Date('Employment Visa End Date')
+     emirates_id_no = fields.Char(string="Emirates ID No")
+     emirates_id_expiration = fields.Date('Emirates ID Expiration')
+     health_insurance_number = fields.Integer('Health Insurance Number')
+     health_insurance_expiry = fields.Date('Health Insurance Expiry')
+
+
+
+     religion = fields.Char(string="Religion")
+     personal_email_address = fields.Char("Personal Email Address")
+     personal_contact_no = fields.Char(string='Personal Contact No')
+     personal_whatsapp_no = fields.Char("Personal Whatsapp No")
+
+     attachment_ids = fields.Many2many(comodel_name='ir.attachment', string='Attachments', tracking=True)
+
+     
+     
+
+    
+
