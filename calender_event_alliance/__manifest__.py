746d537c175af7565bc921116c2952facda05bec
Hamayun fazal <hamayunfazal1@gmail.com>
2024-01-02 11:33:39 +0000

    changes in calender module

--- calender_event_alliance/__manifest__.py
+++ calender_event_alliance/__manifest__.py
@@ -24,7 +24,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
          'security/security.xml',
         'views/location.xml',
         'views/calender.xml',
