da44e3f9e7f68304321605ed3cb663a3c5fe0af0
hamayunfazal <hamayunfazal1@gmail.com>
2024-02-22 13:33:05 +0000

    changes in sale report

--- sale_custom/__manifest__.py
+++ sale_custom/__manifest__.py
@@ -15,7 +15,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'sale',
-    'version': '15.0.0.22',
+    'version': '15.0.0.23',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'sale_management', 'crm' ,'sale' ,'hr', 'job_estimate','sale_project'],
